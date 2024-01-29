42c7f3a9593ed54e137e998e9c1a32d91bd4f26b
Camillefranca <66768430+Camillefranca@users.noreply.github.com>
2024-01-29 16:22:00 -0300

    Create sorteio_aluno.py

---
+++ sorteio_aluno.py
@@ -0,0 +1,10 @@
+#Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
+import random
+
+alu1 = str(input('Primeiro aluno: '))
+alu2 = str(input('Segundo aluno: '))
+alu3 = str(input('Terceiro aluno: '))
+alu4 = str(input('Quarto aluno: '))
+
+lista = [alu1, alu2, alu3, alu4]
+print('O aluno escolhido foi {}'.format(random.choice(lista)))
